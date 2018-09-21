f564de7c164d052a4ee9fc6f67fb2a2ba0f59a3f
kevintudiep <kevintudiep@gmail.com>
2018-09-21 15:36:13 -0500

    Completed First Web_App

--- server.rb
+++ server.rb
@@ -1,10 +1,29 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
 
 get '/home' do
   erb :index
 end
 
-get '/portfolio' do 
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
+get '/portfolio' do
   erb :gallery
+end
+
+get '/about_me' do
+  @skills = ['shuffling', 'ilvl 380 holy priest', 'funny', 'can eat a lot']
+  @interests = ['techno', 'women (+ points if youre japanese)', 'games']
+  erb :about
+end
+
+get '/favourites' do
+  @fav_links = ['www.youtube.com', 'www.reddit.com', 'www.netflix.com', 'www.totallysfwsite.com']
+  erb :favourites
 end
